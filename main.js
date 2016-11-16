13a460b61e5785bf3c6df2cc58c92860a348e326
Daniel Espeset <daniel@danielespeset.com>
2016-11-15 22:59:30 -0500

    2.2.1 2.2.2 2.2.3 onFulfilled and onRejected args

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function Prom(exec) {
-    this.status = Prom.statusTypes.PENDING
+    this.status = Prom.statusTypes.PENDING;
     this.value = undefined;
     this.catcher = function(err){
-        console.error("(in promise) " + err);
+        // console.error("(in promise) " + err);
     };
-    this.queue = [];
+    this.yayQueue = [];
+    this.nayQueue = [];
     exec && exec(this.resolve.bind(this), this.reject.bind(this));
 }
 
@@ -26,27 +27,30 @@ Prom.prototype.reject = function(reason) {
     this.catcher(reason);
 };
 
-Prom.prototype._add = function(fn) {
-    this.queue.push(fn);
-};
-
 Prom.prototype._runQueue = function() {
-    if (this.status === Prom.statusTypes.RESOLVED) {
-        while (this.queue.length) {
+    if (this.status !== Prom.statusTypes.PENDING) {
+        var q = this.status === Prom.statusTypes.RESOLVED ? this.yayQueue : this.nayQueue;
+        while (q.length) {
             if (this.value instanceof Prom) {
-                this.value.then(this.queue.pop()).catch(this.catcher);
+                this.value.then(q.shift()).catch(this.catcher);
             } else {
-                this.queue.pop()(this.value);
+                var fn = q.shift();
+                var val = this.value;
+                setTimeout((function(fn, val){ return function(){ fn(val); } })(fn, val), 0);
             }
         }
     }
 }
 
-Prom.prototype.then = function(fn) {
+Prom.prototype.then = function(yayFn, nayFn) {
     var prom = new Prom();
-    this._add(function(val) { return prom.resolve(fn(val)); });
+    typeof yayFn === 'function' && this.yayQueue.push(function(val) { return prom.resolve(yayFn(val)); });
+    typeof nayFn === 'function' && this.nayQueue.push(function(val) { return prom.reject(nayFn(val)); });
     this.catcher = function (err){ prom.catcher(err); }
     this._runQueue();
+    if (this.status !== Prom.statusTypes.PENDING) {
+        prom[this.status === Prom.statusTypes.REJECTED ? 'reject' : 'resolve'](this.value);
+    }
     return prom;
 };
 
